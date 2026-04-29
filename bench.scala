55436f177d6731d4d066d1f5b23a6a195661b840
Matej Cerny <cerny.matej@gmail.com>
2026-04-29 16:57:03 +0200

    prepare

--- bench.scala
+++ bench.scala
@@ -29,6 +29,7 @@ case class BuildToolDef(
 
 case class ToolBenchmark(
   command: String,
+  prepare: Option[String] = None,
   touch_files: List[String] = Nil
 ) derives YamlDecoder
 
@@ -146,6 +147,7 @@ object Git:
 object Hyperfine:
   def run(
     command: String,
+    prepare: Option[String],
     warmup: Int,
     runs: Int,
     outputJson: os.Path,
@@ -156,8 +158,9 @@ object Hyperfine:
     os.makeDir.all(outputJson / os.up)
     os.write.over(outputJson, "{}")
 
-    println(s">>> Running hyperfine (warmup=$warmup, runs=$runs, timeout=${timeoutMs/60000}m): $command")
-    
+    val prepareDesc = prepare.fold("")(p => s", prepare=$p")
+    println(s">>> Running hyperfine (warmup=$warmup, runs=$runs, timeout=${timeoutMs/60000}m$prepareDesc): $command")
+
     def cleanupStub(): Unit =
       try os.remove(outputJson) catch case _ => ()
 
@@ -168,7 +171,8 @@ object Hyperfine:
         "--warmup", warmup.toString,
         "--runs", runs.toString,
         "--export-json", outputJson.toString
-      ) ++ (if showOutput then Seq("--show-output") else Seq.empty) :+ command
+      ) ++ prepare.toSeq.flatMap(p => Seq("--prepare", p))
+        ++ (if showOutput then Seq("--show-output") else Seq.empty) :+ command
       os.proc(args).call(cwd = workDir, check = false, stdout = os.Inherit, stderr = os.Inherit, timeout = timeoutMs)
     catch
       case e: Exception =>
